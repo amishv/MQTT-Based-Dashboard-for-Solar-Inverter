fa2367906b1d9133c2af28b67f032f8d7f15228b
Amish Vishwakarma <vishwakarma.amish@gmail.com>
2021-12-12 23:41:13 +0530

    Intitial code commit

---
+++ src/mainMQTT.c
@@ -0,0 +1,103 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "solarmon.h"
+#include <MQTTClient.h>
+#include <gtk/gtk.h>
+#include <mqueue.h>
+#include <assert.h>
+
+static MQTTClient mqttClient;
+//static MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+volatile MQTTClient_deliveryToken deliveredtoken;
+static int quit_thread = 0;
+void msgDelivered(__attribute__((unused)) void *context, MQTTClient_deliveryToken dt)
+{
+    debugPrint("Message with token value %d delivery confirmed\n", dt);
+    deliveredtoken = dt;
+}
+int msgArrvd(__attribute__((unused)) void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{ 
+    if (topicLen <0)
+        return -1;
+    if (strstr(topicName, "/state"))
+    {
+        char *mqtBuff = (char *)malloc(sizeof(char) * (strlen(topicName) + message->payloadlen + 3));
+        if (mqtBuff == NULL)
+        {
+            perror("Cannot allocate memory");
+            return -1;
+        }
+        sprintf(mqtBuff, "%s %s", topicName, (char *)message->payload);
+        puts(mqtBuff);
+        mqd_t mqd = mq_open("/SOLARMON_MQ", O_WRONLY, 0600, NULL);
+        /* Ensure the creation was successful */
+        if (mqd == -1)
+        {
+            perror("mq_open");
+            exit(1);
+        }
+        /* Send  message with priority 10, then close the queue.
+          Note the size is incremented to include the null byte '\0'. */
+        mq_send(mqd, mqtBuff, strlen(mqtBuff) + 1, 10);
+        //debugPrint("Message : %s \n", mqtBuff);
+        mq_close(mqd);
+        /* Clean up the allocated memory and message queue */
+        if (mqtBuff != NULL)
+        {
+            free(mqtBuff);
+            mqtBuff = NULL;
+        }
+        else
+            fprintf(stderr, "%s:%d > I found a Null buffer in  \a\a\n", __FILE__, __LINE__);
+    }
+    if (message != NULL)
+      MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+void connlost(__attribute__((unused)) void *context, char *cause)
+{
+    debugPrint("\nConnection lost\n");
+    debugPrint("     cause: %s\n", cause);
+    g_thread_exit(0);
+}
+// int mqtt_main(int argc, char *argv[])
+void *mqtt_main(void )
+{
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int mqttStatus;
+    // uint8_t tmpBuf[150];
+    if ((mqttStatus = MQTTClient_create(&mqttClient, ADDRESS, CLIENTID,
+                                        MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        debugPrint("Failed to create client, return code %d\n", mqttStatus);
+        exit(EXIT_FAILURE);
+    }
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    MQTTClient_setCallbacks(mqttClient, NULL, connlost, msgArrvd, msgDelivered);
+    if ((mqttStatus = MQTTClient_connect(mqttClient, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        debugPrint("Failed to connect, return code %d\n", mqttStatus);
+        exit(EXIT_FAILURE);
+    }
+    debugPrint("Connected Successfully to %s\n", ADDRESS);
+    MQTTClient_subscribe(mqttClient, TOPIC, 0);
+    debugPrint("Subscribed to topic %s\nfor client %s using QoS %d\n", TOPIC, CLIENTID, 0);
+    while (quit_thread != 1)
+    {
+    };
+    MQTTClient_disconnect(mqttClient, 0);
+    MQTTClient_destroy(&mqttClient);
+    g_thread_exit(0);
+    return 0;
+}
+
+void quit_mainMQTT(void)
+{
+    quit_thread = 1; // dangerous! But, I am quitting anyway
+}
---
+++ src/worker.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <string.h>
+#include <gtk/gtk.h>
+//#include <pthread.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mqueue.h>
+#include <assert.h>
+
+gboolean setWidgetVal(GtkWidget *widget, double val);
+extern GtkLabel *gridVoltage, *gridFreq, *inverterVoltage, *inverterFreq;
+extern GtkLabel *pvPwrDisp, *pvVoltDisp, *mpptTempDisp, *invTempDisp;
+extern GtkLabel *battVolts, *battCurrent, *InverterMode;
+extern GtkLabel *loadAct, *loadApparent, *gridState;
+extern GtkDrawingArea *pvPwr, *pvVolts, *mpptTemp, *inverterTemp;
+extern GtkAdjustment *pvPwrVal, *pvVoltVal, *mpptTempVal, *invTempVal;
+extern GtkScale *pvPwrScale;
+extern GtkProgressBar *battPercent, *loadPercent;
+
+gboolean
+time_handler(gpointer *mqd_Ptr)
+{
+    mqd_t *mqd = (mqd_t *)mqd_Ptr;
+    struct mq_attr attr = {0l,0l,0l,0l,{0l,0l,0l,0l}};
+    assert (mq_getattr(*mqd, &attr) != -1);
+    char *buffer = calloc(attr.mq_msgsize, 1);
+    assert(buffer != NULL);
+    /* Retrieve message from the queue and get its priority level */
+    unsigned int priority = 0;
+    if ((mq_receive(*mqd, buffer, attr.mq_msgsize, &priority)) != -1)
+    {
+        char *topic = NULL;
+        char *payload = NULL;
+        topic = strtok(buffer, " ");
+        payload = strtok(NULL, " ");
+        char *ptr;
+        double ret;
+        ret = strtod(payload, &ptr);
+        if (strstr(topic, "AC_grid_voltage/state"))
+        {
+            gtk_label_set_label(gridVoltage, payload);
+        }
+        else if (strstr(topic, "AC_grid_frequency/state"))
+        {
+            gtk_label_set_label(gridFreq, payload);
+        }
+        else if (strstr(topic, "AC_out_voltage/state"))
+        {
+            gtk_label_set_label(inverterVoltage, payload);
+        }
+        else if (strstr(topic, "AC_out_frequency/state"))
+            gtk_label_set_label(inverterFreq, payload);
+        else if (strstr(topic, "PV_in_power/state"))
+        {
+            // gtk_label_set_label(pvPwrDisp, payload);
+            gtk_adjustment_set_value(pvPwrVal, ret);
+        }
+        else if (strstr(topic, "PV_in_voltage/state"))
+        {
+            // gtk_label_set_label(pvVoltDisp, payload);
+            gtk_adjustment_set_value(pvVoltVal, ret);
+        }
+        else if (strstr(topic, "MPPT_temperature/state"))
+        {
+            // gtk_label_set_label(mpptTempDisp, payload);
+            gtk_adjustment_set_value(mpptTempVal, ret);
+        }
+        else if (strstr(topic, "Heatsink_temperature/state"))
+        {
+            // gtk_label_set_label(invTempDisp, payload);
+            gtk_adjustment_set_value(invTempVal, ret);
+        }
+        else if (strstr(topic, "Battery_capacity/state"))
+        {
+            gtk_progress_bar_set_fraction(battPercent, ret / 100);
+        }
+        else if (strstr(topic, "Battery_voltage/state"))
+        {
+            gtk_label_set_label(battVolts, payload);
+        }
+        else if (strstr(topic, "Battery_charge_current/state"))
+        {
+            gtk_label_set_label(battCurrent, payload);
+            if (ret > 0)
+                gtk_widget_set_name(GTK_WIDGET(battCurrent), "label_green");
+            else
+                gtk_widget_set_name(GTK_WIDGET(battCurrent), " ");
+        }
+        else if (strstr(topic, "Battery_discharge_current/state"))
+        {
+            gtk_label_set_label(battCurrent, payload);
+            if (ret > 0) // I rely on receiving charge current before discharge current
+                gtk_widget_set_name(GTK_WIDGET(battCurrent), "label_red");
+        }
+        else if (strstr(topic, "Load_watt/state"))
+        {
+            gtk_label_set_label(loadAct, payload);
+        }
+        else if (strstr(topic, "Load_va/state"))
+        {
+            gtk_label_set_label(loadApparent, payload);
+        }
+        else if (strstr(topic, "Load_pct/state"))
+        {
+            gtk_progress_bar_set_fraction(loadPercent, ret / 100);
+        }
+        else if (strstr(topic, "Inverter_mode/state"))
+        {
+            gtk_label_set_label(InverterMode, payload);
+            if (strstr( payload, "Hybrid"))
+                gtk_widget_set_name(GTK_WIDGET(InverterMode), "label_green");
+            else if (strstr( payload, "Battery"))
+                gtk_widget_set_name(GTK_WIDGET(InverterMode), "label_yellow");
+            else
+                gtk_widget_set_name(GTK_WIDGET(InverterMode), "label_red");
+        }
+        else if (strstr(topic, "AC_Power_dir/state"))
+        {
+            gtk_label_set_label(gridState, ptr);
+            if (strstr( payload, "Output"))
+                gtk_widget_set_name(GTK_WIDGET(gridState), "label_green");
+            else if (strstr( payload, "Input"))
+                gtk_widget_set_name(GTK_WIDGET(gridState), "label_yellow");
+            else
+                gtk_widget_set_name(GTK_WIDGET(gridState), "label_red");
+        }
+        topic = NULL; // dont leave the pointer dangling 
+        payload = NULL;
+        mqd = NULL;
+    }
+    /* Clean up the allocated memory and message queue */
+    if (buffer != NULL)
+    {
+        free(buffer);
+        buffer = NULL;
+    }
+    else
+        fprintf(stderr, "%s:%d > I found a Null buffer in  \a\a\n", __FILE__, __LINE__);
+    /* Return true so the function will be called again; returning false removes
+     * this timeout function.
+     */
+    return TRUE;
+}
+// end worker.c
